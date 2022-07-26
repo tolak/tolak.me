fade5c02076e3bd30d90245a1f361f11e06c3393
Wenfeng Wang <kalot.wang@gmail.com>
2022-07-26 16:31:34 +0800

    contract me

--- bootstrap.js
+++ bootstrap.js
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-social/bootstrap-social.css"
 
 import("./pkg").then(module => {
     module.run_app();
--- src/lib.rs
+++ src/lib.rs
@@ -55,34 +55,51 @@ impl Component for Resume {
         let link = ctx.link();
         html! {
             <>
-            <div class="container">
+            <div class="container mb-5">
                 <div class="row">
                     <div class="col-sm">
                     </div>
-                    <div class="col-sm-6">
-                        // <button onclick={link.callback(|_| Action::FollowTwitter)}>{ "Follow" }</button>
-                        <div class="card border-light">
-                            <div class="card-header border-light">{ "About me" }</div>
-                            <div class="row">
-                                <div class="col"></div>
-                                <div class="col-6">
-                                    <img class="card-img-top rounded-circle z-depth-2" src="/images/morty.jpg" width="180" height="180" data-holder-rendered="true" />
-                                </div>
-                                <div class="col"></div>
-                            </div>
-                            <div class="card-body">
-                                <h5 class="card-title">{ "Wenfeng Wang | tolak.eth" }</h5>
-                                <p class="card-text">{ "Core developer @PhalaNetwork, and https://subbridge.io, writing Rust/Solidity, building Off-Chain&Multi-Chain technologies." }</p>
-                                <div class="btn-group row">
+                    <div class="col-sm-6 bg-light">
+                        // About me
+                        <div class="mt-5">
+                            <i class="fa fa-quote-left fa-2x fa-pull-left fa-border" aria-hidden="true"></i>
+                            <h2>{ "About me" }</h2>
+                        </div>
+                        // Header image
+                        <div class="row mt-5 mb-2">
+                            <div class="col mx-auto text-center">
+                                <img class="rounded-circle z-depth-2" src="/images/morty.jpg" width="180" height="180" data-holder-rendered="true" />
+                                <h5 class="mt-2">{ "Wenfeng Wang | tolak.eth" }</h5>
+                                <div class="btn-group mt-2 row">
                                     <div class="col">
-                                        <a href="https://twitter.com/tolak_eth" target="_blank" class="btn btn-dark" width="120px">{ "Twitter" }</a>
+                                        <a href="https://twitter.com/tolak_eth" target="_blank" class="btn btn-social-icon btn-twitter"><i class="fa fa-twitter"></i></a>
                                     </div>
                                     <div class="col">
-                                        <a href="https://github.com/tolak" target="_blank" class="btn btn-dark" width="120px">{ "Github" }</a>
+                                        <a href="https://github.com/tolak" target="_blank" class="btn btn-social-icon btn-github"><i class="fa fa-github"></i></a>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        <span class="mt-4 text-secondary">
+                            { "Core developer @PhalaNetwork, and " }
+                            <a href="https://subbridge.io" target="_blank"  class="text-primary">{ "https://subbridge.io" }</a>
+                            {", writing Rust/Solidity, building Off-Chain&Multi-Chain technologies." }
+                        </span>
+
+                        // Contact me
+                        <div class="mt-5 mb-4">
+                            <i class="fa fa-quote-left fa-2x fa-pull-left fa-border" aria-hidden="true"></i>
+                            <h2>{ "Contact me" }</h2>
+                        </div>
+                        <form>
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="emailaddressinput" placeholder="name@example.com"/>
+                            </div>
+                            <div class="mt-4 form-group">
+                                <textarea class="form-control" id="messageinput" rows="3"></textarea>
+                            </div>
+                            <button type="submit" class="mt-4 btn btn-primary">{ "Send" }</button>
+                        </form>
                     </div>
                     <div class="col-sm"></div>
                 </div>
