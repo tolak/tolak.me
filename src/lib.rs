bc402bcb94b7f3551f9ca3424741dd0938ceb502
Wenfeng Wang <kalot.wang@gmail.com>
2022-07-27 21:43:24 +0800

    update my resume

--- src/lib.rs
+++ src/lib.rs
@@ -60,11 +60,13 @@ impl Component for Resume {
                     <div class="col-sm">
                     </div>
                     <div class="col-sm-6 bg-light">
+
                         // About me
                         <div class="mt-5">
                             <i class="fa fa-quote-left fa-2x fa-pull-left" aria-hidden="true"></i>
                             <h2>{ "About me" }</h2>
                         </div>
+
                         // Header image and social buttons
                         <div class="row mt-5 mb-2">
                             <div class="col mx-auto text-center">
@@ -80,12 +82,39 @@ impl Component for Resume {
                                 </div>
                             </div>
                         </div>
+
                         // Description
-                        <span class="mt-4 text-secondary">
-                            { "Core developer @PhalaNetwork, and " }
-                            <a href="https://subbridge.io" target="_blank"  class="text-primary">{ "https://subbridge.io" }</a>
-                            {", writing Rust/Solidity, building Off-Chain&Multi-Chain technologies." }
-                        </span>
+                        <div class="mt-4">
+                            <div>
+                                <span class="text-secondary">
+                                    { "Hi, my name is Wenfeng Wang, from China. I’m a software engineer, a blockchain enthusiast, and a Web3 builder. Now I am a core developer of " }
+                                    <a href="https://phala.network" target="_blank"  class="text-primary">{ "Phala Network" }</a>
+                                    {", a Web3 computation cloud network, leading the development of " }
+                                    <a href="https://subbridge.io" target="_blank"  class="text-primary">{ "SubBridge." }</a>
+                                    <br />
+                                </span>
+                            </div>
+                            <div class="mt-2">
+                                <span class="mt-4 text-secondary">
+                                    { "I'm living in Chengdu, a western Chinese city having fabulous food and a fully inclusive culture. In 2016 I graduated from XiHua University with a bachelor of Mechanical and Electronic Engineering. During college, I spent most of my time in the lab, designing MCU controllers and writing code." }
+                                    <br />
+                                </span>
+                            </div>
+                            <div class="mt-2">
+                                <span class="mt-4 text-secondary">
+                                    { "After graduating from college, I joined Sunplus as a system engineer, dedicated to implementing and maintaining a POSIX library running on an embedded system. After leaving Sunplus, I started my journey in the blockchain world. I was attracted immediately when I first know this kind of technology which can bring TRUST to our world through computers rather than human beings. Since that, I have never stopped building Web3 technology for this world, from my first " }
+                                    <a href="https://github.com/tolak/TolakCoin" target="_blank"  class="text-primary">{ "ERC20 smart contract" }</a>
+                                    { " to the building block that I am contributing now." }
+                                    <br />
+                                </span>
+                            </div>
+                            <div class="mt-2">
+                                <span class="mt-4 text-secondary">
+                                    { "I am always interested in technologies that facilitate software development and enhance the real world. Feel free to contact me through email if you wanna discuss more." }
+                                    <br />
+                                </span>
+                            </div>
+                        </div>
 
                         // Contact me
                         <div class="mt-5 mb-4">
